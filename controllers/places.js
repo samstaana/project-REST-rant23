6922161c1d9329e3aaa68dc0a5c74333c86b0c90
KS Durazo <sdurazo.UNLV@gmail.com>
2023-06-21 01:31:28 -0700

    fixed why nodemon was crashing

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,7 @@
-const router  = require('express').Router;
+const router  = require('express').Router();
 
 router.get('/', (req, res) => {
     res.send('GET /places')
 })
 
-modeule.exports = router;
-controllers/places.js
+module.exports = router;
--- index.js
+++ index.js
@@ -3,12 +3,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// The first argument to app.use, /places sests all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller.
+app.use('places', require('./controllers/places'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+// It is important that this stays at the bottom , other wise it will override your other pages!!!
 app.get('*', (req, res) => {
-    res.status(404) .send(`<h1>404 Page</h1>`)
+    res.status(404).send('<h1>404 Page</h1>')
 })
+
 
 app.listen(process.env.PORT)
