c3913f6c5b1339e713c124dcec0860b5fe7c1c4b
KS Durazo <sdurazo.UNLV@gmail.com>
2023-06-24 21:44:57 -0700

    adding home page view

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // The first argument to app.use, /places sests all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller.
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
 
 // It is important that this stays at the bottom , other wise it will override your other pages!!!
