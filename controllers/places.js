858b3c8d3b2433df3d92724a28cb6673844f54a3
KS Durazo <sdurazo.UNLV@gmail.com>
2023-06-25 13:18:46 -0700

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,21 @@
 const router  = require('express').Router();
 const places = require('../models/places')
 
+// INDEX
 router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
+
+// POST
+router.post('places', (req, res) => {
+    res.render('/places')
+})
+
+// NEW
+router.get('/', (req, res) => {
+    res.render('places/new')
+})
+
+
 
 module.exports = router;
--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-rant</h1>
                 <div>
                     <img src="/images/pizza.jpg" alt="Pizza Slice" />
                     <div>
