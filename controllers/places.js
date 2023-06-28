13434136dc055b3df1504683eae2b2a6ea6d4084
KS Durazo <sdurazo.UNLV@gmail.com>
2023-06-27 20:07:04 -0700

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -28,6 +28,15 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+// SHOW
 
+router.get('/:id', (req, res) =>{
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+    res.render('places/show', { place: places [id] })
+    }
+})
 
 module.exports = router;
