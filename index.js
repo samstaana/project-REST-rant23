2fd6efeef1d4fe9fb98db5b546abf40220ec7a54
KS Durazo <sdurazo.UNLV@gmail.com>
2023-07-09 12:36:00 -0500

    fixing routes

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(methodOverride('_method'))
 // What does this do?  When we send data with the POST verb, that data get encryptef for its trip across the internet.  Because it is protected this way in transit, that makes it extra safe for usernames, passwords, and other sensitive data.  However, it also means w will need as extra tool to decrypt that data for us.
 
 // The first argument to app.use, /places sests all routes in the places controller relative to /places. This means that /places will be added in front of any other path we define in the controller.
+
 // Controllrs & Routes
 app.use('/places', require('./controllers/places'))
 
--- models/places.js
+++ models/places.js
@@ -1,26 +1,12 @@
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
-  }]
-  module.exports = [{
-    name: 'Din Tai Fung',
-    city: 'Las Vegas',
-    state: 'NV',
-    cuisines: 'Chinese, Dim-Sum',
-    pic: '/images/dintaifung.jpg'
-  }, {
-    name: 'Carbone',
-    city: 'Las Vegas',
-    state: 'NV',
-    cuisines: 'Italian',
-    pic: '/images/pasta.jpg'
-  }]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
