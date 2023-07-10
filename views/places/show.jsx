198157d0b89d46774fd6dd07c336290b8d675d92
KS Durazo <sdurazo.UNLV@gmail.com>
2023-07-10 14:13:47 -0500

    finished part 9.2

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -8,6 +8,9 @@ function show (data) {
             <div className="row">
                 <div className="col-sm-6">
                     <img src={data.place.pic} alt={data.place.name} />
+                    <h3>
+                        Located in {data.place.city}, {data.place.state}
+                    </h3>
                 </div>
                 <div className="col-sm-6">
                     <h1>{ data.place.name }</h1>
@@ -22,7 +25,7 @@ function show (data) {
                         Description
                     </h2>
                     <h3>
-                        Located in {data.place.city}, {data.place.state}
+                        { data.place.showEstablished() }
                     </h3>
                     <h3>
                         Serving {data.place.cuisines}
